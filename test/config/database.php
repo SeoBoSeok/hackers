a7cad293275164ffb4e6559697edaf9ce2f66afd
SeoBoSeok <ggy7bbo@gmail.com>
2017-12-06 09:18:57 +0900

    apply to gitignore config!!

--- test/config/database.php
+++ test/config/database.php
@@ -1,10 +1,15 @@
 <?php
 
-error_reporting(E_ALL^ E_NOTICE); 
-ini_set("display_errors", 1); 
-@session_start(); 
-$connect = mysql_connect('localhost', 'root', 'localhost'); 
-mysql_query("set names 'euckr'"); 
-mysql_select_db('test' ,$connect);
+	$servername = "localhost";
+	$username = "root";
+	$password = "localhost";
+	$dbname = "test";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
 
 ?>
--- test/member/register/ajax_checkid.php
+++ test/member/register/ajax_checkid.php
@@ -1,18 +1,6 @@
 <?php
 
-	$servername = "localhost";
-	$username = "root";
-	$password = "localhost";
-	$dbname = "test";
-
-	$mb_id = trim($_POST['name']);
-
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
+	include_once('../../config/database.php');
 
 	$sql = "SELECT name FROM members WHERE name LIKE 'ggybbo'";
 	$result = $conn->query($sql);
@@ -20,29 +8,14 @@
 	if ($result->num_rows > 0) {
     // output data of each row
     	while($row = $result->fetch_assoc()) {
-        // echo "id: " . $row["name"]. "<br>";
-    		echo true;
+        	echo "id: " . $row["name"]. "<br>";
+    		// echo true;
+    		if ($row["name"] == $mb_id)
+    			echo false;
     	}
 	} else {
     	// echo "0 results";
-    	echo false;
+    	echo true;
 	}
+
 	$conn->close();
-
-	// $mb_id = trim($_POST['name']);
-
-	// $id_list = array(
-	// 	array("id"=>'1111', "tel"=>'1111'),
-	// 	array("id"=>'2222', "tel"=>'2222'),
-	// 	array("id"=>'3333', "tel"=>'3333'),
-	// 	array("id"=>'4444', "tel"=>'4444'),
-	// 	array("id"=>'5555', "tel"=>'5555'),
-	// );
-
-	$id_list = array(1111, 2222, "3333", "4444", "5555");
-
-	if (isset($_POST['name'])) {
-
-		echo in_array($mb_id, $id_list);
-
-    }
